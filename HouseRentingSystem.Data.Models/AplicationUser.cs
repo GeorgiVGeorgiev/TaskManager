cc531d6953631014d7a467c19d0dbc8e55beb1cf
Георги Георгиев <georgiev.georgi1996@gmail.com>
2023-08-05 13:03:49 +0300

    CreateDbAndAddUsers

---
+++ HouseRentingSystem.Data.Models/AplicationUser.cs
@@ -0,0 +1,24 @@
+﻿namespace HouseRentingSystem.Data.Models
+{
+    using Microsoft.AspNetCore.Identity;
+    using System.ComponentModel.DataAnnotations;
+    using static TaskManager.Common.EntityValidationConstants.User;
+
+    public class ApplicationUser : IdentityUser<Guid>
+    {
+        public ApplicationUser()
+        {
+            this.Id = Guid.NewGuid();
+        }
+
+        [Required]
+        [MaxLength(FirstNameMaxLength)]
+        public string FirstName { get; set; } = null!;
+
+        [Required]
+        [MaxLength(LastNameMaxLength)]
+        public string LastName { get; set; } = null!;
+
+        public ICollection<GeoTask> myTask { get; set; } = new HashSet<GeoTask>();
+    }
+}
---
+++ TaskManager.Common/EntityValidationConstants.cs
@@ -0,0 +1,14 @@
+﻿namespace TaskManager.Common
+{
+    public static class EntityValidationConstants
+    {
+        public static class User
+        {
+            public const int FirstNameMaxLength = 15;
+            public const int FirstNameMinLength = 3;
+
+            public const int LastNameMaxLength = 15;
+            public const int LastNameMinLength = 3;
+        }
+    }
+}
