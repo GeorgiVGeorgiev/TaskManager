e179cd0abdacea7f8458f7b8193bb6527ca66863
Георги Георгиев <georgiev.georgi1996@gmail.com>
2024-07-13 22:37:52 +0300

    Add Calendar Scheduler

--- TaskManager.Services.Data/CalendarService.cs
+++ TaskManager.Services.Data/CalendarService.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TaskManager.Services.Data
+﻿namespace TaskManager.Services.Data
 {
-    internal class CalendarService
+    public class CalendarService
     {
+
     }
 }
